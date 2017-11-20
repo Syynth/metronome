f057439df0fbf78fbe8060a2328234f70b26f06e
Syynth <ben.cochrane2112@gmail.com>
2017-11-20 16:14:02 -0500

    fdghfg

---
+++ Assets/Code/InputController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Code
+{
+
+    public interface IInputSource
+    {
+
+        bool GetButton(string button);
+        bool GetButtonDown(string button);
+        float GetAxis(string axis);
+        Vector2 GetAxis2D(string xAxis, string yAxis);
+
+    }
+
+    public class InputController : MonoBehaviour
+    {
+
+        Stack<IInputSource> InputSources = new Stack<IInputSource>();
+
+        // Use this for initialization
+        void Start()
+        {
+            InputSources.Push(GetComponent<IInputSource>());
+        }
+
+        public bool GetButton(string button)
+        {
+            return InputSources.Peek().GetButton(button);
+        }
+
+        public bool GetButtonDown(string button)
+        {
+            return InputSources.Peek().GetButtonDown(button);
+        }
+
+        public float GetAxis(string axis)
+        {
+            return InputSources.Peek().GetAxis(axis);
+        }
+
+        public Vector2 GetAxis2D(string xAxis, string yAxis)
+        {
+            return InputSources.Peek().GetAxis2D(xAxis, yAxis);
+        }
+
+    }
+
+
+}
---
+++ Assets/Code/Player/RewiredInputController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+namespace Assets.Code.Player
+{
+
+    public class RewiredInputController : MonoBehaviour
+    {
+
+
+        
+    }
+
+
+}
