5ff1f3ceda3386e83374bab2b5829926e9df2252
Syynth <ben.cochrane2112@gmail.com>
2017-11-26 16:39:44 -0500

    Fixes duck and walk animations

--- Assets/Code/Player/PlayerActorLookAhead.cs
+++ Assets/Code/Player/PlayerActorLookAhead.cs
@@ -10,6 +10,7 @@ namespace Assets.Code.Player
 
 		PlayerActor actor;
 		public Vector2 damping = new Vector2(2f, 8f);
+        public Vector2 maxLookahead = new Vector2(6, 12);
 		
 		void Start ()
 		{
@@ -21,7 +22,9 @@ namespace Assets.Code.Player
 			var pos = transform.localPosition;
 			pos.x = actor.velocity.x / damping.x;
 			pos.y = actor.velocity.y / damping.y;
-			transform.localPosition = pos;	
+            pos.x = Mathf.Sign(pos.x) * Mathf.Min(maxLookahead.x, Mathf.Abs(pos.x));
+            pos.y = Mathf.Sign(pos.y) * Mathf.Min(maxLookahead.y, Mathf.Abs(pos.y));
+            transform.localPosition = pos;	
 		}
 
 	}
